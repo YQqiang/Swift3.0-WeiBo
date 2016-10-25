5e26dd6e2009d5900f1092d70f0a339374c0410f
yuqiangsun <yq1054572107@163.com>
2016-10-25 16:40:07 +0800

    简单实用tableView

--- SwiftWeiBo/SwiftWeiBo/Classes/Home/HomeViewController.swift
+++ SwiftWeiBo/SwiftWeiBo/Classes/Home/HomeViewController.swift
@@ -10,12 +10,29 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    //mark: 懒加载
+    lazy var tableView: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
-
+    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        view.backgroundColor = UIColor.init(colorLiteralRed: Float(arc4random()%255)/255.0, green: Float(arc4random()%254)/255.0, blue: Float(arc4random()%255)/255.0, alpha: 1.0)
+//        printLog(messsage: 123)
+//    }
+    
+    func printLog<T>(messsage : T, file : String = #file, funcName : String = #function, lineNum : Int = #line) {
+        
+        #if DEBUG
+            
+            let fileName = (file as NSString).lastPathComponent
+            
+            print("\(fileName):(\(lineNum))-\(messsage)")
+            
+        #endif
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,3 +50,56 @@ class HomeViewController: UIViewController {
     */
 
 }
+
+//mark: 延展
+extension HomeViewController {
+    func setupUI() -> Void {
+        //1.将tableView加到控制器的View
+        view.addSubview(tableView)
+        //2.设置tableview的frame
+        tableView.frame = view.bounds
+        //3.设置数据源和代理
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+//mark: tableView的数据源和代理
+//extension 和 OC中cartegory类似,也是只能扩充方法,不能扩充属性
+extension HomeViewController: UITableViewDataSource,UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 3
+        } else if section == 1 {
+            return 5
+        } else {
+            return 8
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //1.创建cell
+        let cellID = "cell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if cell == nil {
+            cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: cellID)
+        }
+        //2.给cell数据
+        cell?.textLabel?.text = "第\(indexPath.section)组,第\(indexPath.row)行"
+        //3.返回cell
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        printLog(messsage: "点击了第\(indexPath.row)行")
+    }
+}
+
+
+
+
+
