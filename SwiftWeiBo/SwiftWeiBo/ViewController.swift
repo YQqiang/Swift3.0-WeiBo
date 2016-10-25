2edeeee44195674db0a3a4341ddf5ac6038731d3
yuqiangsun <yq1054572107@163.com>
2016-10-25 14:57:09 +0800

    创建项目工程

---
+++ SwiftWeiBo/SwiftWeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWeiBo
+//
+//  Created by kjlink on 2016/10/25.
+//  Copyright © 2016年 kjlink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
