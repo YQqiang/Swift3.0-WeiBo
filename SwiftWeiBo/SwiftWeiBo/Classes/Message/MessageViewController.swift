70ee950f9883046650d668adb7b3ddd9d7aabbcc
yuqiangsun <yq1054572107@163.com>
2016-10-26 08:52:29 +0800

    随便写了一点内容,测试

--- SwiftWeiBo/SwiftWeiBo/Classes/Message/MessageViewController.swift
+++ SwiftWeiBo/SwiftWeiBo/Classes/Message/MessageViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class MessageViewController: UIViewController {
 
+    lazy var redView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+//        let redView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 100, height: 100))
+        redView.center = view.center
+        redView.backgroundColor = UIColor.red
+        view.addSubview(redView)
+        redView.layer.cornerRadius = 5.0
+        redView.layer.borderColor = UIColor.cyan.cgColor
+        redView.layer.borderWidth = 2.0
+        
+        let x = 20.0
+        let margin = 20.0
+        let count = 4.0
+        let buttonW = (Double(view.frame.size.width) - 2.0 * x - (count - 1.0) * margin) / count
+        let titles = ["平移", "放大", "缩小", "旋转"]
+        for i in 0 ..< Int(count) {
+            let button = UIButton.init(frame: CGRect.init(x: x + ((margin + buttonW) * Double(i)), y: 64.0, width: buttonW, height: 44.0))
+            button.backgroundColor = UIColor.blue
+            button.setTitle(titles[i], for: UIControlState.normal)
+            button.tag = 1000 + i
+            button.addTarget(self, action: #selector(clickButton(sender:)), for: UIControlEvents.touchUpInside)
+            view.addSubview(button)
+        }
+    }
+    
+    func clickButton(sender: UIButton) -> Void {
+//        switch sender.tag - 1000 {
+//        case 0:
+//            redView.transform = cga
+//        case 1:
+//            
+//        case 2:
+//            
+//        case 3:
+//            
+//        default:
+//            print("other")
+//        }
     }
 
     override func didReceiveMemoryWarning() {
